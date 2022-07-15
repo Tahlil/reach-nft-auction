b8145d8351a6c294f2931d6ee1db5e1ebd030615
Tahlil <bsse0803@iit.du.ac.bd>
2022-07-15 23:02:42 +0600

    Creata an nft

--- index.mjs
+++ index.mjs
@@ -5,3 +5,17 @@ const stdlib = loadStdlib();
 const startingBalance = stdlib.parseCurrency(100);
 
 console.log("Creating a test account creator");
+
+const accCreator = await stdlib.newTestAccount(startingBalance);
+
+console.log("Creator create a testnet NFT");
+
+const theNFT = await stdlib.launchToken(accCreator, "mystic", "NFT", {supply: 3} );
+const nftId = theNFT.id;
+const minBid = stdlib.parseCurrency(2);
+const lenInBlocks = 9;
+
+const params = {nftId, minBid, lenInBlocks};
+
+
+
